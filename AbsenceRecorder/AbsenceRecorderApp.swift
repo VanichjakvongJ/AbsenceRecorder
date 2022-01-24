ef8e3c57c20ad10044b8f212abf33b4e83991572
Jakpat Vanichjakvong <Vanichjakvong.J@etoncollege.org.uk>
2022-01-24 18:29:44 +0000

    Initial Commit

---
+++ AbsenceRecorder/AbsenceRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbsenceRecorderApp.swift
+//  AbsenceRecorder
+//
+//  Created by Vanichjakvong, Jakpat (IRG) on 24/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct AbsenceRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
